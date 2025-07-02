a1918bfa1efe7a4274a39c11f7a1d7783a2301f8
Franck-Sorel <mfrancksorel@gmail.com>
2025-07-02 08:54:03 +0100

    fix: 404 error message from the root has been fix.

--- api/handler.rs
+++ api/handler.rs
@@ -1,21 +1,59 @@
-use serde_json::json;
-
-use vercel_runtime::{Body, Error, Request, Response, StatusCode, run};
+use vercel_runtime::{Body, Error, Request, Response, run};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     run(handler).await
 }
 
-pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+async fn handler(req: Request) -> Result<Response<Body>, Error> {
+    println!("[REQUEST: {:?}]", req);
+    println!("[URI: {:?}]", req.uri());
+    println!("[PATH: {:?}]", req.uri().path());
+
+    let path = req.uri().path();
+    println!("Path of the request... : {path}");
+    println!("Path of the request... : {path}");
+    // Default behavior on root path
+
+    let doc: &str = r#"{
+      "project": "Vercel Rust API",
+      "description": "This project exposes two main serverless API endpoints using Vercel and Rust.",
+      "routes": [
+        {
+          "path": "/api",
+          "file": "handler.rs",
+          "method": "GET",
+          "description": "Root endpoint of the API. Returns a welcome message.",
+          "example_request": "curl https://my-vercel-api-alpha.vercel.app/api",
+          "example_response": "Welcome to my root API"
+        },
+        {
+          "path": "/api/fibbot",
+          "file": "fibbot.rs",
+          "method": "GET",
+          "description": "Computes the Fibonacci number for the value passed through the 'name' query parameter.",
+          "query_parameters": [
+            {
+              "name": "name",
+              "type": "integer",
+              "required": true,
+              "description": "The number for which to calculate the Fibonacci value"
+            }
+          ],
+          "example_request": "curl \"https://my-vercel-api-alpha.vercel.app/api/fibbot?name=10\"",
+          "example_response": {
+            "message": "你好，世界",
+            "fib_number": 55
+          },
+          "notes": "The 'name' parameter must be a valid non-negative integer, if not i might default on 0"
+        }
+      ]
+    }
+    "#;   
     Ok(Response::builder()
-        .status(StatusCode::OK)
-        .header("Content-Type", "application/json")
-        .body(
-            json!({
-              "message": "你好，世界, Hello!",
-            })
-            .to_string()
-            .into(),
-        )?)
+        .status(200)
+        .header("content-type", "application/json")
+        .body(format!("{}", doc).into())
+        .unwrap())
 }
+
